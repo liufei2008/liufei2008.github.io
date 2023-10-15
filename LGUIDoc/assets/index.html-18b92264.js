559b77cead19325b5ed8315a22d20c3c44d4b132
lf2007hf <707908214@qq.com>
2023-10-15 09:35:45 +0800

    version to 3.4.0

--- LGUIDoc/assets/index.html-18b92264.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as t,c as r,b as e,d as n}from"./app-3f775d0d.js";const c={},o=e("h1",{id:"prefab-variant",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#prefab-variant","aria-hidden":"true"},"#"),n(" Prefab Variant")],-1),s=[o];function d(i,_){return t(),r("div",null,s)}const h=a(c,[["render",d],["__file","index.html.vue"]]);export{h as default};
---
+++ LGUIDoc/assets/index.html-af4db9e8.js
@@ -0,0 +1,1 @@
+import{_ as a,o as t,c as r,b as e,d as n}from"./app-c68c2f04.js";const c={},o=e("h1",{id:"prefab-variant",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#prefab-variant","aria-hidden":"true"},"#"),n(" Prefab Variant")],-1),s=[o];function d(i,_){return t(),r("div",null,s)}const h=a(c,[["render",d],["__file","index.html.vue"]]);export{h as default};
